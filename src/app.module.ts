4dd8984938027603844293cb6a3fd40a1baf4fb9
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-07-10 08:42:23 -0300

    [ADD and UPD] Acabo de instalar o  prisma no projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { SMTPMailService } from './mail/smtp-mail.service';
 import { MailService } from './mail/mail.service';
+import { HttpModule } from './http.module';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [
     {
