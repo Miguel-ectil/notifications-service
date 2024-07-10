2199e1767abfc8602ed9c414bf03f9f4de65456e
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-07-10 17:45:54 -0300

    [ADD and UPD]Acabo de configurar o jest, teste unitaria das funciionalidades, e já criei meus primeiros testes

---
+++ src/application/entities/content.spec.ts
@@ -0,0 +1,15 @@
+import { Content } from './content';
+
+test('it should be able to create a notification content', () => {
+  const content = new Content('Você recebeu uma solicitação de amizade');
+
+  expect(content).toBeTruthy();
+});
+
+test('it should not be able to create a notification content with less than 5 characters', () => {
+  expect(() => new Content('aaa')).toThrow();
+});
+
+test('it should not be able to create a notification content with more than 240 characters', () => {
+  expect(() => new Content('a'.repeat(241))).toThrow();
+});
