730affd1fbb00118af79d3d1c3ce2cb0850c224b
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-07-10 11:35:26 -0300

    [ADD] riando requisição post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
 
 @Controller('notifications')
 export class AppController {
@@ -11,7 +12,15 @@ export class AppController {
   }
 
   @Post()
-  create() {
-    return this.prisma.notification.findMany();
+  async create(@Body() body: any) {
+    const { recipientId, content, category } = body;
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
